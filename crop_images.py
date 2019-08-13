0e0ff8253784fbc95f5b60bfd92aeb5040f435d7
joseph <moore.joseph@gmail.com>
2019-08-13 02:02:23 -0400

    refactored code for concurrency, increased speed by 4x

--- crop_images.py
+++ crop_images.py
@@ -3,10 +3,12 @@ import argparse
 import os
 from glob import glob
 from PIL import Image
+import concurrent.futures
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--source', help='image or directory of images to be cropped', required=True) 
-parser.add_argument('-t', '--ftype', help='image file type, e.g. jpg png')
+parser.add_argument('-t', '--ftype', help='image file type, e.g. jpg png, only required if dest=dir')
 parser.add_argument('-c', '--coords', help='crop coords: left, top, right, bottom', nargs='+', type=int, required=True)
 parser.add_argument('-d', '--dest', help='image or directory of images to save crop to', required=True)
 args = parser.parse_args()
@@ -14,15 +16,20 @@ args = parser.parse_args()
 src = args.source
 ftype = args.ftype
 dst = args.dest
-coords = tuple(args.coords)
+COORDS = tuple(args.coords)
 
 #coords = (489,269,759,458)
 
-def crop_image(src_img, coords):
+def crop_image(src_img):
+    im = Image.open(src_img)
+    im_crop = im.crop(COORDS)
+    im_crop.save(dst)
+
+def crop_multi_image(src_img):
     image_name = os.path.basename(src_img)
     im = Image.open(src_img)
-    im_crop = im.crop(coords)
-    return im_crop
+    im_crop = im.crop(COORDS)
+    im_crop.save(dst + image_name)
 
 def get_image_paths(src_dir, ftype):
     img_list = []
@@ -30,16 +37,19 @@ def get_image_paths(src_dir, ftype):
         img_list.append(f)
     return img_list
 
+def crop_all_images(img_list):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        executor.map(crop_multi_image, img_list)
+
 def main():
     if os.path.isdir(src):
         img_list = get_image_paths(src, ftype)
-        for img_path in img_list:
-            img = crop_image(img_path, coords)
-            img.save(dst + os.path.basename(img_path))
-
+        crop_all_images(img_list)
     elif os.path.isfile(src):
-        img = crop_image(src, coords)
-        img.save(dst)
+        crop_image(src)
 
 if __name__ == "__main__":
+    start_time= time.time()
     main()
+    duration = time.time() - start_time
+    print(f"finished in {duration} seconds.")
